a6d6ba6d2e51013f37523f38fbbfbc84a5864080
charsyam <charsyam@gmail.com>
2012-12-26 22:44:23 -0800

    add bottle.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='guoid',
+      version='0.1',
+      description='Python SnowFlake',
+      author='DaeMyung Kang',
+      author_email='charsyam@gmail.com',
+      url='https://github.com/charsyam/python-quoid',
+      platforms='any',
+      packages=['snowflake']
+      )
